cbc2442bcccd24509501ea0a7c4cdc1de83522fd
Lali98 <kisslajosbertalan77@gmail.com>
2023-03-16 08:58:54 +0100

    2023.03.16.

--- src/Components/newAd.js
+++ src/Components/newAd.js
@@ -1,5 +1,14 @@
-function NewAd() {
+import {useEffect, useState} from "react";
 
+function NewAd() {
+    const [categories, setCategory] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/api/kategoriak', {
+            "Content-Type": "application/json"
+        })
+            .then((data) => data.json())
+            .then(data => setCategory(data));
+    }, []);
     return (
         <div className="container">
             <h2 className="mb-4 text-center">Új hirdetés elküldése</h2>
@@ -8,26 +17,22 @@ function NewAd() {
                     <div className="mb-3">
                         <label htmlFor="category" className="form-label">Ingatlan kategóriája</label>
                         <select className="form-select" name="kategoriaId">
-                            <option value="0">Kérem válasszon</option>
-                            <option value="1">Ház</option>
-                            <option value="2">Lakás</option>
-                            <option value="3">Építési telek</option>
-                            <option value="4">Garázs</option>
-                            <option value="5">Mezőgazdasági terület</option>
-                            <option value="6">Ipari ingatlan</option>
+                            {categories.map((category) => (
+                                <option value={category.id}>{category.megnevezes}</option>
+                            ))}
                         </select>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="date" className="form-label">Hirdetés dátuma</label>
-                        <input type="date" readOnly className="form-control" name="hirdetesDatuma"/>
+                        <input type="date" className="form-control" name="hirdetesDatuma" readOnly value={new Date().toISOString().split('T')[0]}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Ingatlan leírása</label>
                         <textarea className="form-control" name="leiras" rows="3"></textarea>
                     </div>
                     <div className="form-check mb-3">
-                        <input className="form-check-input" type="checkbox" name="tehermentes" checked/>
+                        <input className="form-check-input" type="checkbox" name="tehermentes"/>
                         <label className="form-check-label" htmlFor="creditFree">Tehermentes ingatlan</label>
                     </div>
                     <div className="mb-3">
@@ -35,14 +40,12 @@ function NewAd() {
                         <input type="url" className="form-control" name="kepUrl"/>
                     </div>
                     <div className="mb-3 text-center">
-                        <button className="btn btn-primary px-5">Küldés</button>
+                        <button className="btn btn-primary px-5" type='submit'>Küldés</button>
                     </div>
-
                     <div className="alert alert-danger alert-dismissible" role="alert">
                         <strong>Hiba szövege</strong>
                         <button type="button" className="btn-close"></button>
                     </div>
-
                 </div>
             </div>
         </div>
